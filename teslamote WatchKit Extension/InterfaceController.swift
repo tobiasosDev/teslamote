904f9fe91cb66d4c110f800018471a4f2a8f86bd
tobiasosDev <luescher.tobias1@gmail.com>
2019-04-26 10:59:33 +0200

    Initial Commit Xcode

---
+++ teslamote WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  teslamote WatchKit Extension
+//
+//  Created by Tobias Lüscher on 26.04.19.
+//  Copyright © 2019 Tobias Lüscher. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ teslamote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teslamote
+//
+//  Created by Tobias Lüscher on 26.04.19.
+//  Copyright © 2019 Tobias Lüscher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
